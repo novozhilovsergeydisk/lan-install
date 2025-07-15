87653eec99ad392c5b22a4ecf6b7616b3aaa7670
novozhilovsergeydisk <novozhilovsergeydisk@yandex.ru>
2025-07-16 02:37:52 +0300

    ui: обновлены цвета таблицы состава бригады в модальном окне
    Изменения:
    - Стили таблицы теперь используют значения rgb() вместо hex
    - Обновлены цвета фона и текста для улучшения контраста
    - Улучшена читаемость данных о бригаде в модальном окне

--- public/js/handler.js
+++ public/js/handler.js
@@ -3219,18 +3219,18 @@ function handlerCreateBrigade() {
                                                     border: 1px solid #000000 !important;
                                                 }
                                                 .brigade-info-container .table thead th {
-                                                    background-color: #e0e0e0 !important;
-                                                    color: #000000 !important;
+                                                    background-color:rgb(28, 25, 25) !important;
+                                                    color:rgb(246, 234, 234) !important;
                                                     border-bottom: 2px solid #000000 !important;
                                                     font-weight: bold !important;
                                                 }
                                                 .brigade-info-container .table-striped > tbody > tr:nth-of-type(odd) > * {
-                                                    background-color: #f0f0f0 !important;
-                                                    color: #000000 !important;
+                                                    background-color:rgb(66, 64, 64) !important;
+                                                    color:rgb(222, 205, 205) !important;
                                                 }
                                                 .brigade-info-container .table-striped > tbody > tr:nth-of-type(even) > * {
-                                                    background-color: #ffffff !important;
-                                                    color: #000000 !important;
+                                                    background-color:rgb(77, 75, 75) !important;
+                                                    color:rgb(245, 232, 232) !important;
                                                 }
                                                 .brigade-info-container .table td, .brigade-info-container .table th {
                                                     padding: 0.5rem !important;
