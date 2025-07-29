7e9fa653f2f67176fff453fc3acf62c38d6838fd
novozhilovsergeydisk <novozhilovsergeydisk@yandex.ru>
2025-07-29 12:54:12 +0300

    Убрано кеширование браузера

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,5 @@
 @php
     // Заголовки для отключения кэширования
-    <!-- header("Cache-Control: no-cache, no-store, must-revalidate");
-    header("Pragma: no-cache");
-    header("Expires: 0"); -->
 
     // Функция для сокращения ФИО до фамилии и первой буквы имени
     function shortenName($fullName) {
