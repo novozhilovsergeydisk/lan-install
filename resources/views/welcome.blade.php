97d3bd67aab51e685ccb8cc695de001152afd65a
novozhilovsergeydisk <novozhilovsergeydisk@yandex.ru>
2025-09-04 22:33:50 +0300

    feat(validation): расширена валидация формата номера дома
    - Добавлена поддержка формата с указанием только строения (7, строение 1)
    - Обновлено регулярное выражение для валидации
    - Улучшено отображение подсказок с примерами форматов
    - Добавлен перенос строки для лучшей читаемости списка форматов
    - Обновлены сообщения об ошибках для большей ясности

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2901,13 +2901,14 @@
                                class="form-control" 
                                placeholder="Введите номер дома в соответствии с форматом" 
                                data-required="true"
-                               pattern="^\d+[A-Za-zА-Яа-я]*(?:,\s*корпус\s+\d+[A-Za-zА-Яа-я]*(?:,\s*строение\s+\d+[A-Za-zА-Яа-я]*)?)?$"
+                               pattern="^\d+[A-Za-zА-Яа-я]*(?:,\s*(?:корпус\s+\d+[A-Za-zА-Яа-я]*(?:,\s*строение\s+\d+[A-Za-zА-Яа-я]*)?|строение\s+\d+[A-Za-zА-Яа-я]*))?$"
                                title="Введите номер дома в соответствии с форматом">
                         <div class="form-text text-muted">
                             Формат: <span class="text-info">7</span> или 
                                     <span class="text-info">7A</span> или 
-                                    <span class="text-info">7, корпус 2</span> или 
-                                    <span class="text-info">7, корпус 2, строение 1</span>
+                                    <span class="text-info">7, корпус 2</span> или<br> 
+                                    <span class="text-info">7, корпус 2, строение 1</span> или
+                                    <span class="text-info">7, строение 1</span>
                         </div>
                         <div class="invalid-feedback d-none">Пожалуйста, укажите корректный номер дома</div>
                     </div>
