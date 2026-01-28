6123cad29b304a81b97c61b1f7733a3828d2f418
novozhilovsergeydisk <novozhilovsergeydisk@yandex.ru>
2026-01-28 18:26:53 +0300

    chore: добавлена задача очистки старых архивов
      Детальное описание:
      В планировщик задач (routes/console.php) добавлена ежемесячная команда для автоматического удаления временных zip-архивов, созданных более 30 дней
      назад, чтобы предотвратить переполнение диска.

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Очистка временных архивов раз в месяц (удаляем файлы старше 30 дней)
+Schedule::exec('find ' . storage_path('app/temp') . ' -type f -mtime +30 -delete')
+    ->monthly()
+    ->appendOutputTo(storage_path('logs/cleanup.log'));
+
