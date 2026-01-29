0a35a14e938953cf73e3233200fd02e2b48db78f
novozhilovsergeydisk <novozhilovsergeydisk@yandex.ru>
2026-01-29 22:37:01 +0300

    feat: улучшено форматирование и дизайн истории заявок для бота
    - Исправлен рендеринг HTML-тегов в комментариях для корректного отображения форматирования (жирный шрифт, переносы).
    - Переработана структура таблицы в reports.address-history для компактности и информативности.
    - Добавлены адаптивные стили для удобного просмотра отчетов с мобильных устройств.

--- resources/views/reports/address-history.blade.php
+++ resources/views/reports/address-history.blade.php
@@ -73,7 +73,7 @@
                                                         @foreach($commentsByRequest[$request->id] as $comment)
                                                             <div class="mb-2 p-2 bg-white border rounded shadow-sm" style="font-size: 0.85rem; line-height: 1.3;">
                                                                 <div class="mb-1 text-break">
-                                                                    {!! nl2br(e($comment->comment)) !!}
+                                                                    {!! $comment->comment !!}
                                                                 </div>
                                                                 <div class="d-flex justify-content-between text-muted border-top pt-1 mt-1" style="font-size: 0.7rem;">
                                                                     <span>{{ \Carbon\Carbon::parse($comment->created_at)->format('d.m.Y H:i') }}</span>
